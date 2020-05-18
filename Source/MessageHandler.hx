f2cb840c5a7d0f44192d6c800747b6242df01b94
Apprentice Alchemist <azzer159@yahoo.com>
2020-05-18 20:01:10 +0200

    Fix

--- Source/MessageHandler.hx
+++ Source/MessageHandler.hx
@@ -1,39 +1,39 @@
 package;
 
-import haxicord.DiscordClient;
-import haxicord.types.Message;
+// import haxicord.DiscordClient;
+// import haxicord.types.Message;
 
 class MessageHandler {
-    var bot:DiscordClient;
-    public function new(_bot:DiscordClient){
-        bot = _bot;
-    }
+    // var bot:DiscordClient;
+    // public function new(_bot:DiscordClient){
+    //     bot = _bot;
+    // }
 
-    public static function handle(m:Message){
-		if(m.content.substring(0, Bot.prefix.length) == Bot.prefix){
-            CommandHandler.handle(m);
-            return;
-        } else {
-            // handleMessage(m);
-            return;
-        }
+    // public static function handle(m:Message){
+	// 	if(m.content.substring(0, Bot.prefix.length) == Bot.prefix){
+    //         CommandHandler.handle(m);
+    //         return;
+    //     } else {
+    //         // handleMessage(m);
+    //         return;
+    //     }
         
-    }
-    public static function handleMessage(m:Message):Dynamic{
-        var content = m.content;
-        if(StringTools.contains(content.toLowerCase(),"good bot")){
-            m.reply({content: "Thanks, I do my best!"});
-            trace("message handled");
-            return true;
-		} else if (StringTools.contains(content.toLowerCase(), "bad bot")){
-            m.reply({content: "Sorry, I'll do better next time."});
-            trace("message handled");
-			return true;
-        } else if(m.mentions.lastIndexOf(Bot.bot.user) > -1){
-            m.reply({content: "Hello, did you call me? If you want to know what I can do type `]help`!"});
-            return true;
-        }
-        trace("message handled");
-		return false;
-    }
+    // }
+    // public static function handleMessage(m:Message):Dynamic{
+    //     var content = m.content;
+    //     if(StringTools.contains(content.toLowerCase(),"good bot")){
+    //         m.reply({content: "Thanks, I do my best!"});
+    //         trace("message handled");
+    //         return true;
+	// 	} else if (StringTools.contains(content.toLowerCase(), "bad bot")){
+    //         m.reply({content: "Sorry, I'll do better next time."});
+    //         trace("message handled");
+	// 		return true;
+    //     } else if(m.mentions.lastIndexOf(Bot.bot.user) > -1){
+    //         m.reply({content: "Hello, did you call me? If you want to know what I can do type `]help`!"});
+    //         return true;
+    //     }
+    //     trace("message handled");
+	// 	return false;
+    // }
 }
